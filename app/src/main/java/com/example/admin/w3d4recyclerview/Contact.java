cfe68ee7baa93370a35a3ee5467c0979fb269ed6
eguegan <e.guegan@margulls.com>
2016-04-14 17:39:37 -0400

    First commit

---
+++ app/src/main/java/com/example/admin/w3d4recyclerview/Contact.java
@@ -0,0 +1,36 @@
+package com.example.admin.w3d4recyclerview;
+
+import java.util.ArrayList;
+
+/**
+ * Created by admin on 4/14/2016.
+ */
+public class Contact {
+    private String mName;
+    private boolean mOnline;
+
+    public Contact(String name, boolean online) {
+        mName = name;
+        mOnline = online;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public boolean isOnline() {
+        return mOnline;
+    }
+
+    private static int lastContactId = 0;
+
+    public static ArrayList<Contact> createContactsList(int numContacts) {
+        ArrayList<Contact> contacts = new ArrayList<Contact>();
+
+        for (int i = 1; i <= numContacts; i++) {
+            contacts.add(new Contact("Person " + ++lastContactId, true));
+        }
+
+        return contacts;
+    }
+}
---
+++ app/src/main/java/com/example/admin/w3d4recyclerview/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.admin.w3d4recyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Contact> contacts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // ...
+        // Lookup the recyclerview in activity layout
+        RecyclerView rvContacts = (RecyclerView) findViewById(R.id.rvContacts);
+
+        // Initialize contacts
+        contacts = Contact.createContactsList(20);
+        // Create adapter passing in the sample user data
+        ContactsAdapter adapter = new ContactsAdapter(contacts);
+        // Attach the adapter to the recyclerview to populate items
+        rvContacts.setAdapter(adapter);
+        // Set layout manager to position the items
+        rvContacts.setLayoutManager(new LinearLayoutManager(this));
+        // That's all!
+    }
+}
